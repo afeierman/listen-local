62875169a84016b4b7c7b695e54b0444e6f54088
Andrew Feierman <Andrew.Feierman@gmail.com>
2017-06-05 22:34:40 -0400

    general cleanup to prepare for better build out

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 import json
-from flask import Flask, request, redirect, render_template, jsonify
 import requests
 import os
 import base64
 import urllib
-import spotipy
-import pandas as pd
 
-# from concert_finder import run_listen_local
+import pandas as pd
+from flask import Flask, request, redirect, render_template, jsonify
+
+import spotipy
+
+from listen_local import *
 
 # Authentication Steps, parameters, and responses are defined at https://developer.spotify.com/web-api/authorization-guide/
 # Visit this url to see all the steps, parameters, and expected response.
@@ -15,7 +17,7 @@ import pandas as pd
 app = Flask(__name__)
 
 #use a secret key to set our session...?
-app.secret_key = ''
+app.secret_key = 'longsecretkey'
 
 #Concert data, scraped from songkick.com
 concerts = pd.read_csv("~/listen-local/app/concerts_clean.csv", index_col=0, encoding='utf-8')
@@ -131,65 +133,7 @@ def create_playlist():
     except Exception as e:
         return jsonify(result="Uh oh, something went wrong. Did you fill in the date range and select a venue?" + "\n Error: " + str(e))
 
-#concert retrieval functions below
 
-def get_venue_artists(venue, start_date = "2017-01-01", end_date = "2017-12-31"):
-    concert_dates = pd.date_range(start = start_date, end = end_date, freq = 'D')
-    artists = []
-    for show in concerts.loc[concerts['venue'] == venue, ['date', 'artist']].itertuples():
-        if show[1] in concert_dates:
-            artists.append(show)
-    return set(artists)
-
-def get_artist_ids(artists):
-    sp = spotipy.Spotify()
-    artist_plus_ids = []
-    for artist in artists:
-        search = sp.search(q=artist, type = 'artist', limit = 1)
-        try:
-            artist_plus_ids.append((search['artists']['items'][0]['name'],  search['artists']['items'][0]['id']))
-        except IndexError:
-            pass
-    return set(artist_plus_ids)
-
-def get_venue_artist_ids(venue, start_date = "2017-01-01", end_date = "2017-12-31"):
-    sp = spotipy.Spotify()
-    artists = get_venue_artists(venue, start_date, end_date)
-    artist_plus_ids = []
-    for artist in artists:
-        search = sp.search(q=artist[2], type = 'artist', limit = 1)
-        try:
-            artist_plus_ids.append((search['artists']['items'][0]['name'] ,  search['artists']['items'][0]['id']))
-        except IndexError:
-            pass
-    return set(artist_plus_ids)
-
-def create_venue_songlist(venue, start_date = "2017-01-01", end_date = "2017-12-31"):
-    sp = spotipy.Spotify()
-    songlist = []
-    num_tracks = 5
-    if (start_date == "2017-01-01") and (end_date == "2017-12-31"):
-        num_tracks = 3
-    for artist in get_venue_artist_ids(venue, start_date, end_date):
-        artist_tracks = sp.artist_top_tracks(artist[1])['tracks']
-        if len(artist_tracks) >= num_tracks:
-            for track in range(0, num_tracks):
-                songlist.append((artist_tracks[track]['name'], artist_tracks[track]['id']))
-    return songlist
-
-def prepare_song_id_list(songlist):
-    song_ids = []
-    for song in songlist:
-        song_ids.append(str(song[1]))
-    return song_ids
-
-def create_venue_songlist_ids(venue, start_date = "2017-01-01", end_date = "2017-12-31"):
-    songlist = create_venue_songlist(venue, start_date, end_date)
-    #the max length for playlists created via the spotify API is 100 songs, so truncate the song list if it's too long
-    if len(songlist) >= 100:
-        songlist = songlist[0:99]
-    ptitle = ("%s from %s to %s") % (venue, start_date, end_date)
-    return (ptitle, prepare_song_id_list(songlist))
 
 #create_venue_songlist_ids returns a tuple with the playlist title, and a list of Spotify song IDs
 #main function below (this is what the webapp calls)
